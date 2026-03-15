1a788b48621e4d88303c2fabd9af603620a326c5
Mohd Fikrie <fikrie8@gmail.com>
2026-03-16 01:56:12 +0800

    Added inbound Model,Repo, Controller, Service, DAO,

---
+++ backend/fikrie/src/main/java/com/example/fikrie/Controller/InboundController.java
@@ -0,0 +1,47 @@
+package com.example.fikrie.Controller;
+
+import com.example.fikrie.DataTransferObject.RequestRespondInbound;
+import com.example.fikrie.DataTransferObject.RequestRespondInventory;
+import com.example.fikrie.Service.InboundService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/inbound")
+public class InboundController {
+
+    @Autowired
+    InboundService inboundService;
+
+    @GetMapping("/get-by-reference")
+    public ResponseEntity<RequestRespondInbound> getInboundByReference(@RequestBody RequestRespondInbound requestRespondInbound) {
+        return ResponseEntity.ok(inboundService.getInboundByReference(requestRespondInbound.getReference()));
+    }
+
+    @GetMapping("/get-by-sku")
+    public ResponseEntity<RequestRespondInbound> getInboundBySku(@RequestBody RequestRespondInbound requestRespondInbound) {
+        return ResponseEntity.ok(inboundService.getInboundBySku(requestRespondInbound.getProductSku()));
+    }
+
+    @GetMapping("/get-all")
+    public ResponseEntity<RequestRespondInbound> getAllInbound() {
+        return ResponseEntity.ok(inboundService.getAllInbound());
+    }
+
+    @PostMapping("register")
+    public ResponseEntity<RequestRespondInbound> registerInbound(@RequestBody RequestRespondInbound requestRespondInbound) {
+        return ResponseEntity.ok(inboundService.registerInbound(requestRespondInbound));
+    }
+
+    @PutMapping("/update/{requestInboundId}")
+    public ResponseEntity<RequestRespondInbound> updateInbound(@PathVariable(required = true) Long requestInboundId, @RequestBody RequestRespondInbound requestUpdateInbound) {
+        return ResponseEntity.ok(inboundService.updateInbound(requestInboundId, requestUpdateInbound));
+    }
+
+    @DeleteMapping("/delete/{requestInboundId}")
+    public ResponseEntity<RequestRespondInbound> deleteInbound(@PathVariable(required = true) Integer requestInboundId) {
+        return ResponseEntity.ok(inboundService.deleteInbound(Long.valueOf(requestInboundId)));
+    }
+}
---
+++ backend/fikrie/src/main/java/com/example/fikrie/DataTransferObject/RequestRespondInbound.java
@@ -0,0 +1,29 @@
+package com.example.fikrie.DataTransferObject;
+
+import com.example.fikrie.Model.Inbound;
+import com.example.fikrie.Model.Inventory;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+
+import java.util.Date;
+import java.util.List;
+
+@Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class RequestRespondInbound {
+
+    private int statusCode;
+    private String reference;
+    private Date dateReceived;
+    private String productSku;
+    private int quantity;
+    private String location;
+    private String remarks;
+    private String message;
+    private String error;
+    private List<Inbound> ListOfInbound;
+}
